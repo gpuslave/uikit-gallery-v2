f0b6cf7ece9837a8b10ca5246af3fcc9d0fb4ada
Aleksei Nemtsev <shimanoee@yandex.ru>
2025-10-16 09:42:25 +0300

    Initial Commit

---
+++ gallerry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gallerry
+//
+//  Created by Aleksei Nemtsev on 16.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
